9cb964188ca65d107a0a0fc89a9e7b7b1acb8993
jericirenej <63595376+jericirenej@users.noreply.github.com>
2023-08-31 20:05:01 +0200

    Initial setup of svelte and db related packages.

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:storybook/recommended",
+    "prettier",
+    "plugin:svelte/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+    project: ["./tsconfig.json", "./db/tsconfig.json"],
+    tsconfigRootDir: __dirname,
+    extraFileExtensions: [".svelte"]
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser"
+      }
+    }
+  ]
+};
