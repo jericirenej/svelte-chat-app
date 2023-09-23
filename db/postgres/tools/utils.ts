4dfbd999c68dbb9b1f34b4f50ceec03dc8271bb7
jericirenej <63595376+jericirenej@users.noreply.github.com>
2023-09-23 21:53:14 +0200

    Add uniqueUUID utility function

--- db/postgres/tools/utils.ts
+++ db/postgres/tools/utils.ts
@@ -1,4 +1,5 @@
 import { exec } from "child_process";
+import { randomUUID } from "crypto";
 import { promisify } from "util";
 
 export const asyncExec = async (
@@ -13,3 +14,18 @@ export const asyncExec = async (
 export const typedJsonClone = <T>(obj: T): T => JSON.parse(JSON.stringify(obj)) as T;
 
 export const randomPick = <T>(arr: T[]): T => arr[Math.floor(Math.random() * arr.length)];
+
+/** UUID's should never match, but just in case, let's make sure and create a unique one. */
+export const uniqueUUID = (references: string[]): string => {
+  let newUUID = "";
+  let counter = 0;
+  while (!newUUID || references.includes(newUUID)) {
+    newUUID = randomUUID();
+    // Safety check
+    if (counter > 100) {
+      throw new Error("Error creating unique UUID");
+    }
+    counter++;
+  }
+  return newUUID;
+};
