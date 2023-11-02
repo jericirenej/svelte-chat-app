6ad952f70b9a085832c461cb40004baaf312b516
jericirenej <63595376+jericirenej@users.noreply.github.com>
2023-11-02 19:37:36 +0100

    Base input component

---
+++ src/components/atomic/Input/Input.stories.svelte
@@ -0,0 +1,83 @@
+<script context="module" lang="ts">
+  import type { Meta } from "@storybook/svelte";
+  import Input from "./Input.svelte";
+
+  export const meta: Meta<Input> = {
+    title: "Atomic/Input",
+    component: Input,
+    argTypes: {
+      name: { control: "text" },
+      type: {
+        control: "select",
+        options: [
+          "button",
+          "checkbox",
+          "color",
+          "date",
+          "datetime-local",
+          "email",
+          "file",
+          "hidden",
+          "image",
+          "month",
+          "number",
+          "password",
+          "radio",
+          "range",
+          "reset",
+          "search",
+          "submit",
+          "tel",
+          "text",
+          "time",
+          "url",
+          "week"
+        ]
+      },
+      placeholder: { control: "text" },
+      disabled: { control: "boolean" },
+      label: { control: "text" },
+      containerWidth: { control: { type: "range", min: 10, max: 100, step: 5 } }
+    }
+  };
+</script>
+
+<script lang="ts">
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+  import type { ComponentProps } from "svelte";
+  const componentKeys = ["name", "type", "label", "disabled", "placeholder"],
+    wrapperKeys = ["containerWidth"];
+  const handleArgs = <T extends Record<string, unknown>>(args: T) => {
+    return {
+      props: componentKeys.reduce((obj, key) => {
+        obj[key] = args[key];
+        return obj;
+      }, {} as ComponentProps<Input>),
+      container: wrapperKeys.reduce((obj, key) => {
+        obj[key] = args[key];
+        return obj;
+      }, {} as Record<string, unknown>)
+    };
+  };
+</script>
+
+<Template let:args>
+  {#if args}
+    {@const { props, container } = handleArgs(args)}
+    <div style:width={`${container.containerWidth}%`}>
+      <Input {...props} />
+    </div>
+  {/if}
+</Template>
+
+<Story
+  name="Primary"
+  args={{
+    disabled: false,
+    type: "text",
+    label: "Username",
+    name: "username",
+    placeholder: "Enter your username",
+    containerWidth: 50
+  }}
+/>
---
+++ src/components/atomic/Input/Input.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import type { HTMLInputTypeAttribute } from "svelte/elements.js";
+
+  export let name: string;
+  export let type: HTMLInputTypeAttribute;
+  export let label: string;
+  export let placeholder: string;
+  export let disabled: boolean = false;
+</script>
+
+<div class="relative bg-inherit">
+  <input
+    class="peer text-sm px-2 h-10 w-full outline-none border-2 border-neutral-400 text-neutral-900 rounded placeholder-transparent
+    focus:border-violet-400 focus:shadow-xs focus:shadow-violet-500
+    invalid:border-red-600"
+    id={`${name}-input`}
+    {name}
+    {type}
+    placeholder={placeholder ?? label}
+    {disabled}
+    {...$$props}
+  />
+  {#if label}
+    <label
+      class="absolute z-10 inline-block cursor-text transition-all bg-white
+      left-2 -top-2 text-neutral-600 text-xs
+      px-2
+      peer-placeholder-shown:top-2.5
+      peer-placeholder-shown:text-sm
+      peer-focus:-top-2 peer-focus:text-xs"
+      for={`${name}-input`}
+      id={`${name}-label`}>{label}</label
+    >
+  {/if}
+</div>
