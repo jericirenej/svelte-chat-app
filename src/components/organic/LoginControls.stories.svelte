ab8cf2de28e37d5cbf2b80b4ef8c7d6def3eb804
jericirenej <63595376+jericirenej@users.noreply.github.com>
2023-11-07 20:10:46 +0100

    LoginControl component create

---
+++ src/components/organic/LoginControls.stories.svelte
@@ -0,0 +1,23 @@
+<script context="module" lang="ts">
+  import type { Meta } from "@storybook/svelte";
+  import LoginForm from "./LoginControls.svelte";
+
+  export const meta: Meta<LoginForm> = {
+    title: "Organic/Login",
+    component: LoginForm,
+    argTypes: {
+      isLoading: { control: "boolean" },
+      status: { control: "radio", options: ["success", "error", undefined] }
+    }
+  };
+</script>
+
+<script lang="ts">
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+</script>
+
+<Template let:args>
+  <LoginForm {...args} />
+</Template>
+
+<Story name="Primary" />
---
+++ src/components/organic/LoginControls.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  import Input from "../atomic/Input/Input.svelte";
+  import SubmitButton from "../molecular/SubmitButton/SubmitButton.svelte";
+
+  export let isLoading = false;
+  export let status: "success" | "error" | undefined = undefined;
+  const message = { success: "Login successful!", error: "Username or password not correct!" };
+</script>
+
+<div class="flex flex-initial flex-col gap-4">
+  <div class="flex flex-col gap-4">
+    <Input label="Username" placeholder="Enter your username" name="username" type="text" />
+    <Input label="Password" placeholder="Enter your password" name="password" type="password" />
+  </div>
+  <div class="relative pb-5">
+    <SubmitButton text="LOGIN" {isLoading} config={{ display: "block" }} />
+    {#if status}
+      <small
+        transition:fade={{ duration: 150 }}
+        class={`
+      absolute bottom-0 text-xs
+      left-[50%] translate-x-[-50%]
+      ${status === "error" ? "text-red-600" : "text-emerald-500"}
+      `}>{message[status]}</small
+      >
+    {/if}
+  </div>
+</div>
