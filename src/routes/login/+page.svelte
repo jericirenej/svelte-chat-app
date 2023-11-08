3758cdaebcf0ea7effaadbfdc6ebd4df8adb5308
jericirenej <63595376+jericirenej@users.noreply.github.com>
2023-11-08 20:57:32 +0100

    Refactor login page to use the new LoginControls organic component.

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,173 +4,61 @@
   import { onMount } from "svelte";
   import { fade } from "svelte/transition";
   import { superForm } from "sveltekit-superforms/client";
+  import LoginControls from "../../components/organic/LoginControls.svelte";
   import { LOCAL_SESSION_CSRF_KEY } from "../../constants.js";
-  import { capitalize } from "../../utils/text-utils.js";
   import type { ActionData, PageData } from "./$types.js";
+  import { loginSchema } from "./login-form-validator.js";
 
   export let data: PageData;
+  let isLoading = false;
   type FormResult<T extends Record<string, unknown> | null> = ActionResult<
     NonNullable<T>,
     NonNullable<T>
   >;
 
-  let showResultMessage = false;
+  let status: undefined | "success" | "error" = undefined;
 
   const handleInput = () => {
-    if (showResultMessage) showResultMessage = false;
+    status = undefined;
   };
 
-  const { form, errors, constraints, enhance, message, validate } = superForm(data.form, {
+  const { form, enhance } = superForm(data.form, {
+    onSubmit: () => {
+      isLoading = true;
+    },
+    validators: loginSchema,
+    customValidity: true,
     onResult(event) {
       const result = event.result as FormResult<Required<ActionData>>;
-      showResultMessage = true;
+      console.log(result.type);
+      isLoading = false;
       if (result.type === "success" && result.data) {
+        status = "success";
         const data = result.data;
         localStorage.setItem(LOCAL_SESSION_CSRF_KEY, data.csrfToken);
+      } else {
+        status = "error";
       }
     }
   });
 
-  const typeAction = (node: HTMLInputElement, type: "text" | "password") => {
-    node.type = type;
-  };
-
-  const formInputs = [
-    { type: "text", name: "username" },
-    { type: "password", name: "password" }
-  ] as const;
-
   onMount(async () => await invalidateAll());
 </script>
 
-<div class="wrapper">
+<div
+  class="absolute top-[30%] left-1/2 -translate-x-1/2 -translate-y-[30%] flex flex-col justify-center items-center"
+>
+  <div class="flex flex-col items-center gap-1 mb-8">
+    <h1 class="text-[2rem] font-light text-sky-800 uppercase leading-7">Sign in</h1>
+    <span class="text-sm font-normal text-sky-800">And start chatting!</span>
+  </div>
   <form method="POST" use:enhance transition:fade>
-    <h1>LOGIN</h1>
-    {#each formInputs as { name, type }}
-      <section>
-        <div class="input-wrapper">
-          <input
-            id={name}
-            {name}
-            use:typeAction={type}
-            aria-label={capitalize(name)}
-            aria-invalid={$errors[name] ? "true" : undefined}
-            bind:value={$form[name]}
-            on:input={handleInput}
-            {...$constraints[name]}
-          />
-          <div aria-hidden={true}>{capitalize(name)}</div>
-          {#if $errors[name]}
-            <small class="invalid-field">{$errors[name]}</small>
-          {/if}
-        </div>
-      </section>
-    {/each}
-    <div class="submit-wrapper">
-      <button type="submit">Login</button>
-      {#if showResultMessage}
-        {#if $errors?._errors?.length}
-          <small class="submission-result invalid">{$errors._errors[0]}</small>
-        {:else if !Object.keys($errors).length}
-          <small class="submission-result valid">Login successful!</small>
-        {/if}
-      {/if}
-    </div>
+    <LoginControls
+      bind:username={$form.username}
+      bind:password={$form.password}
+      {isLoading}
+      onInput={handleInput}
+      {status}
+    />
   </form>
 </div>
-
-<style lang="postcss">
-  .wrapper {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    & > * {
-      font-family: "Poppins", sans-serif;
-    }
-  }
-  form {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    font-size: 13px;
-    padding: 2rem;
-    outline: 1px solid gray;
-    border-radius: 5px;
-    width: 500px;
-  }
-  .submit-wrapper {
-    position: relative;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: end;
-    align-items: center;
-    gap: 1rem;
-  }
-
-  h1 {
-    user-select: none;
-  }
-
-  h1 {
-    margin-top: 0;
-  }
-
-  section {
-    width: 100%;
-    margin-bottom: 15px;
-  }
-
-  .input-wrapper {
-    position: relative;
-  }
-  input {
-    padding-block: 5px;
-    width: 100%;
-  }
-
-  .submission-result {
-    font-size: 11px;
-    position: absolute;
-    top: 110%;
-    text-justify: center;
-    &.invalid {
-      color: hsl(0, 75%, 60%);
-    }
-    &.valid {
-      color: hsl(140deg, 70%, 50%);
-    }
-  }
-  .invalid-field {
-    font-size: 11px;
-    color: hsl(0, 75%, 60%);
-    position: absolute;
-    top: 100%;
-    left: 0;
-  }
-  button {
-    outline: 1px solid var(--almost-black, black);
-    border: none;
-    background-color: white;
-    border-radius: 3px;
-    font-size: 13px;
-    font-weight: 500;
-    letter-spacing: 0.05ch;
-    text-transform: uppercase;
-    color: var(--almost-black, black);
-    width: 100%;
-    padding: 5px 15px;
-    transition: color 0.2s ease-out, background-color 0.2s ease-out;
-    align-self: flex-end;
-    &:hover,
-    &:active,
-    &:focus {
-      background-color: var(--almost-black, black);
-      color: white;
-    }
-  }
-</style>
