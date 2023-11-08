31da55d540c9582d9629a04944c53240de0b50b2
jericirenej <63595376+jericirenej@users.noreply.github.com>
2023-11-08 20:56:39 +0100

    Add props for value binding and input handlers. Minor styling optimization for error message.

--- src/components/organic/LoginControls.svelte
+++ src/components/organic/LoginControls.svelte
@@ -3,6 +3,9 @@
   import Input from "../atomic/Input/Input.svelte";
   import SubmitButton from "../molecular/SubmitButton/SubmitButton.svelte";
 
+  export let username: string;
+  export let password: string;
+  export let onInput: (ev?: Event) => unknown = () => {};
   let message: string | undefined = undefined;
   export let isLoading = false;
   export let status: "success" | "error" | undefined = undefined;
@@ -14,19 +17,34 @@
 
 <div class="flex flex-initial flex-col gap-4">
   <div class="flex flex-col gap-4">
-    <Input label="Username" placeholder="Enter your username" name="username" type="text" />
-    <Input label="Password" placeholder="Enter your password" name="password" type="password" />
+    <Input
+      label="Username"
+      placeholder="Enter your username"
+      name="username"
+      type="text"
+      input={onInput}
+      bind:value={username}
+    />
+    <Input
+      label="Password"
+      placeholder="Enter your password"
+      name="password"
+      type="password"
+      input={onInput}
+      bind:value={password}
+    />
   </div>
   <div class="relative pb-5">
     <div class={isLoading ? "loading relative" : ""}>
-      <SubmitButton text="LOGIN" {isLoading} config={{ display: "block" }} />
+      <SubmitButton text="SUBMIT" {isLoading} config={{ display: "block" }} />
     </div>
     {#if status}
       <small
         transition:fade={{ duration: 150 }}
         class={`
       absolute bottom-0 text-xs
-      left-[50%] translate-x-[-50%]
+      text-center
+      w-[100%]
       ${status === "error" ? "text-red-600" : "text-emerald-500"}
       `}>{message ?? defaultMessage[status]}</small
       >
