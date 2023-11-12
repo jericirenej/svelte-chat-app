17ddf2bcdd96d9fd41f8adcc58149989b802ede2
jericirenej <63595376+jericirenej@users.noreply.github.com>
2023-11-12 08:37:19 +0100

    Don't pass colors that are already included by default. THis removes unnecessary warnings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,8 @@
 import type { Config } from "tailwindcss";
-import colors from "tailwindcss/colors.js";
 
 const config: Config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    colors: { ...colors },
     extend: {}
   },
   plugins: []
