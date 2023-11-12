90f8f2575becad2f1e66f1394b9de47768144951
jericirenej <63595376+jericirenej@users.noreply.github.com>
2023-11-12 12:06:46 +0100

    add max -width custom transition prop

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {}
+    extend: {
+      transitionProperty: {
+        "max-width": "max-width"
+      }
+    }
   },
   plugins: []
 };
