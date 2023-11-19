c593d396afe7402ae0b330eb186c4dbf37165041
jericirenej <63595376+jericirenej@users.noreply.github.com>
2023-11-19 16:45:59 +0100

    Crate NavIcons component and integrate it within the main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { goto, invalidateAll } from "$app/navigation";
+  import { page } from "$app/stores";
   import { fly } from "svelte/transition";
   import "../app.css";
+  import NavIcons from "../components/molecular/NavIcons/NavIcons.svelte";
   import { CSRF_HEADER, LOCAL_SESSION_CSRF_KEY } from "../constants.js";
   import type { LayoutData } from "./$types";
 
@@ -28,18 +30,14 @@
   <div class="app flex h-[95vh] w-[95vw] max-w-[1500px] rounded-md bg-white">
     <section
       transition:fly={{ duration: 300, y: -400 }}
-      class={`sidebar transition-max-width h-full w-full rounded-s-[inherit] bg-slate-700 text-neutral-50 duration-300 ${
-        loggedIn ? "max-w-[250px] pl-2 pr-5" : "max-w-0"
+      class={`sidebar h-full w-full rounded-s-[inherit] bg-slate-700 text-neutral-50 transition-max-width duration-300 ${
+        loggedIn ? "max-w-[250px]" : "max-w-0"
       }`}
     >
       {#if loggedIn}
         <nav>
           <section>
-            <ul>
-              <li><a href="/">Homepage</a></li>
-              <li><a href="/profile" data-sveltekit-preload-data="hover">Profile</a></li>
-              <li><button type="button" on:click|preventDefault={handleLogout}>Logout</button></li>
-            </ul>
+            <NavIcons routeId={$page.route.id} {handleLogout} />
           </section>
         </nav>
       {:else}
