94870d46515ee0fdc9e1958c655c26af7ef2a9e7
jericirenej <63595376+jericirenej@users.noreply.github.com>
2023-11-29 19:23:55 +0100

    Remove console log

--- src/lib/server/authenticate.ts
+++ src/lib/server/authenticate.ts
@@ -12,7 +12,6 @@ export const authenticateUser = async ({
 }): Promise<CompleteUserDto | null> => {
   if (!sessionId) return null;
   const user = await redisService.getSession(sessionId);
-  console.log("SESSION USER", user);
   if (!user) return null;
   if (method === "GET") return user;
   // All non-GET requests, such as POST, PUT, PATCH, or DELETE
@@ -26,7 +25,7 @@ export const authenticateUser = async ({
   return user;
 };
 
-/** Logout the current user. Will nt check if session exists.
+/** Logout the current user. Will not check if session exists.
  * Will not authenticate. This is done by the authentication function called
  * previously in the handle hook. */
 export const logoutUser = async (sessionId: string): Promise<boolean> => {
