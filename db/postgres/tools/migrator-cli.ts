9df1c9e8172fe3771a440ff9fcbf7130d5975501
jericirenej <63595376+jericirenej@users.noreply.github.com>
2023-12-28 12:39:46 +0100

    Os and Linux specific location parsing implemented.

--- db/postgres/tools/migrator-cli.ts
+++ db/postgres/tools/migrator-cli.ts
@@ -1,9 +1,10 @@
 import { CamelCasePlugin, Kysely, Migrator } from "kysely";
 import { dialect } from "../client.js";
 import { ESMFileMigrationProvider, MigrationHelper } from "./migrator.js";
+import { resolveUrlPath } from "./utils.js";
 
 const MIGRATIONS_PATH_RELATIVE = new URL("../migrations", import.meta.url),
-  TYPE_PATH = new URL("../db-types.ts", import.meta.url).pathname.substring(1),
+  TYPE_PATH = resolveUrlPath(new URL("../db-types.ts", import.meta.url)),
   db = new Kysely<unknown>({ dialect, log: ["query"], plugins: [new CamelCasePlugin()] }),
   migrator = new Migrator({ db, provider: new ESMFileMigrationProvider(MIGRATIONS_PATH_RELATIVE) });
 
