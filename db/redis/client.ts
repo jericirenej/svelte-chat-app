1b765cdc450cda524561da11e32a65ccea3862d7
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-01-17 17:46:04 +0100

    Add static service initializer

--- db/redis/client.ts
+++ db/redis/client.ts
@@ -1,7 +1,9 @@
 import { createClient } from "redis";
 import env from "../environment.js";
 
-const clientConnection = () => createClient({ password: env.REDIS_HOST_PASSWORD });
+export type RedisClient = ReturnType<typeof createClient>;
+
+const clientConnection = (): RedisClient => createClient({ password: env.REDIS_HOST_PASSWORD });
 const redisClient = clientConnection();
 
 redisClient.on("error", (err) => {
@@ -9,5 +11,3 @@ redisClient.on("error", (err) => {
 });
 
 export { clientConnection, redisClient };
-
-export type RedisClient = typeof redisClient;
