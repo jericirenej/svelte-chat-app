06dc65fb0ccf8223324f9ffa282c4c4a5ca7053d
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-01-27 20:32:50 +0100

    FormWrapper story created

---
+++ src/components/organic/FormWrapper/FormWrapper.stories.svelte
@@ -0,0 +1,49 @@
+<script context="module" lang="ts">
+  import type { Meta } from "@storybook/svelte";
+  import FormWrapper from "./FormWrapper.svelte";
+
+  export const meta: Meta<FormWrapper> = {
+    title: "Organic/FormWrapper",
+    component: FormWrapper,
+    argTypes: {
+      subtitle: { control: "text" },
+      formTitle: { control: "text" }
+    }
+  };
+</script>
+
+<script lang="ts">
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+  import type { ComponentProps } from "svelte";
+  import SlotFills from "./stroybook-helpers/SlotFills.svelte";
+  const args: Partial<ComponentProps<FormWrapper>> & { footer: boolean } = {
+    formTitle: "Form title",
+    subtitle: "Form subtitle",
+    footer: true
+  };
+
+  const formWrapperArgs = (completeArgs: typeof args): Omit<typeof args, "footer"> => {
+    return Object.fromEntries(Object.entries(completeArgs).filter(([key]) => key !== "footer"));
+  };
+</script>
+
+<Template let:args>
+  {@const formArgs = formWrapperArgs(args)}
+  {#if args.footer}
+    <FormWrapper {...formArgs}>
+      <div slot="form">
+        <SlotFills />
+      </div>
+      <div slot="footer"><SlotFills isFooter={true} /></div>
+    </FormWrapper>
+  {:else}
+    <FormWrapper {...formArgs}>
+      <div slot="form">
+        <SlotFills />
+      </div>
+    </FormWrapper>
+  {/if}
+</Template>
+
+<Story name="Primary" {args} />
+<Story name="No footer" args={{ ...args, footer: false }} />
---
+++ src/components/organic/FormWrapper/stroybook-helpers/SlotFills.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { capitalize } from "../../../../utils/text-utils";
+
+  export let isFooter: boolean = false;
+  $: slotHeight = isFooter ? "min-h-[10ch]" : "min-h-[15ch]";
+</script>
+
+<div
+  class={`flex ${slotHeight} min-w-[30ch] items-center justify-center rounded-md bg-gray-400 text-black`}
+>
+  <span>{isFooter ? "Footer" : "Form"} slot fill</span>
+</div>
