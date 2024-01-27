8a4be52893eb8521698511a184f0044ad9f61c7a
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-01-27 19:47:19 +0100

    Create FormWrapper component and implement it in the login page

---
+++ src/components/organic/FormWrapper/FormWrapper.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let formTitle: string | undefined = undefined;
+  export let subtitle: string | undefined = undefined;
+
+  $: showHeader = formTitle || subtitle;
+</script>
+
+<div
+  class="absolute left-1/2 top-[30%] flex -translate-x-1/2 -translate-y-[30%] flex-col items-center justify-center"
+>
+  {#if showHeader}
+    <div class="mb-8 flex flex-col items-center gap-1">
+      {#if formTitle}
+        <h1 class="text-[2rem] font-light uppercase leading-7 text-sky-800">{formTitle}</h1>
+        {#if subtitle}
+          <span class="text-sm font-normal text-sky-800">{subtitle}</span>
+        {/if}
+      {/if}
+    </div>
+  {/if}
+  <div>
+    <slot name="form" />
+  </div>
+  {#if $$slots.footer}
+    <div class="mt-10">
+      <slot name="footer" />
+    </div>
+  {/if}
+</div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,16 +7,26 @@
   import { fade } from "svelte/transition";
   import { superForm } from "sveltekit-superforms/client";
   import RootHeading from "../../components/atomic/RootHeading/RootHeading.svelte";
+  import FormWrapper from "../../components/organic/FormWrapper/FormWrapper.svelte";
   import LoginControls from "../../components/organic/LoginControls/LoginControls.svelte";
   import { loginSchema } from "../../lib/client/login-signup-validators.js";
   import type { PageData } from "./$types.js";
 
   export let data: PageData;
+
   let isLoading = false;
-
   let submitDisabled = true;
-
   let status: 200 | 404 | undefined = undefined;
+
+  const submitDisabledToggle = debounce(async () => {
+    const { valid } = await validate();
+    submitDisabled = !valid;
+  }, 150);
+
+  const handleInput = async () => {
+    submitDisabledToggle();
+    status = undefined;
+  };
 
   const { form, enhance, validate } = superForm(data.form, {
     onSubmit: async () => {
@@ -34,28 +44,12 @@
     }
   });
 
-  const submitDisabledToggle = debounce(async () => {
-    const { valid } = await validate();
-    submitDisabled = !valid;
-  }, 150);
-
-  const handleInput = async () => {
-    submitDisabledToggle();
-    status = undefined;
-  };
-
   onMount(async () => await invalidateAll());
 </script>
 
 <svelte:head><title>Chat App - Login</title></svelte:head>
-<div
-  class="absolute left-1/2 top-[30%] flex -translate-x-1/2 -translate-y-[30%] flex-col items-center justify-center"
->
-  <div class="mb-8 flex flex-col items-center gap-1">
-    <h1 class="text-[2rem] font-light uppercase leading-7 text-sky-800">Sign in</h1>
-    <span class="text-sm font-normal text-sky-800">...and start chatting!</span>
-  </div>
-  <form method="POST" use:enhance in:fade>
+<FormWrapper formTitle="Sign in" subtitle="...and start chatting!">
+  <form slot="form" method="POST" use:enhance in:fade>
     <LoginControls
       bind:username={$form.username}
       bind:password={$form.password}
@@ -65,7 +59,5 @@
       {submitDisabled}
     />
   </form>
-  <div class="mt-10">
-    <RootHeading />
-  </div>
-</div>
+  <RootHeading slot="footer" />
+</FormWrapper>
