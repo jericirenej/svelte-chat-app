edd22abbb69435e3f4f928eab933309a8e73e252
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-03-22 20:27:15 +0100

    Create FormFooter

---
+++ src/components/molecular/FormFooter/FormFooter.stories.svelte
@@ -0,0 +1,27 @@
+<script context="module" lang="ts">
+  import type { Meta } from "@storybook/svelte";
+  import FormFooter from "./FormFooter.svelte";
+
+  export const meta: Meta<FormFooter> = {
+    title: "Molecular/FormFooter",
+    component: FormFooter,
+    argTypes: {
+      link: { control: "text" },
+      label: { control: "text" }
+    },
+    args: {
+      label: "Follow this link to win great prizes!",
+      link: "https://nowhere.anywhere"
+    }
+  };
+</script>
+
+<script lang="ts">
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+</script>
+
+<Template let:args>
+  <FormFooter {...args} />
+</Template>
+
+<Story name="Primary"/>
