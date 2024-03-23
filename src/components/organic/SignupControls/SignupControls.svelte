3589d2addfab5920a849696941a43f657d20b418
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-03-23 09:35:00 +0100

    Basic signup controls without any form logic created

--- src/components/organic/SignupControls/SignupControls.svelte
+++ src/components/organic/SignupControls/SignupControls.svelte
@@ -1,1 +1,81 @@
-<div>SIGNUP</div>
+<script lang="ts">
+  import { SIGNUP_MESSAGES as MSG } from "../../../messages";
+  import FormSubmitNotification from "../../atomic/FormSubmitNotification/FormSubmitNotification.svelte";
+  import Input from "../../atomic/Input/Input.svelte";
+  import SubmitButton from "../../molecular/SubmitButton/SubmitButton.svelte";
+  import ControlsWrapper from "../../molecular/wrappers/ControlsWrapper/ControlsWrapper.svelte";
+
+  export let username: string;
+  export let password: string;
+  export let email: string;
+  export let name: string;
+  export let surname: string;
+
+  export let onInput: (ev?: Event) => unknown = () => {};
+  export let isLoading = false;
+  export let status: 200 | 404 | undefined = undefined;
+  export let submitDisabled = false;
+
+  const defaultMessage = {
+    200: MSG.signupSuccess,
+    404: MSG.signupFailure
+  };
+</script>
+
+<ControlsWrapper>
+  <svelte:fragment slot="inputs">
+    <Input
+      label="Username"
+      placeholder={MSG.usernamePlaceholder}
+      name="username"
+      type="text"
+      input={onInput}
+      bind:value={username}
+    />
+    <Input
+      label="Email"
+      placeholder={MSG.emailPlaceholder}
+      name="email"
+      type="text"
+      input={onInput}
+      bind:value={email}
+    />
+    <Input
+      label="Password"
+      placeholder={MSG.passwordPlaceholder}
+      name="password"
+      type="password"
+      input={onInput}
+      bind:value={password}
+    />
+    <Input
+      label="Name"
+      placeholder={MSG.namePlaceholder}
+      name="name"
+      type="name"
+      input={onInput}
+      bind:value={name}
+    />
+    <Input
+      label="Surname"
+      placeholder={MSG.surnamePlaceholder}
+      name="surname"
+      type="surname"
+      input={onInput}
+      bind:value={surname}
+    />
+  </svelte:fragment>
+  <svelte:fragment slot="controls">
+    <SubmitButton
+      disabled={submitDisabled}
+      text="SUBMIT"
+      {isLoading}
+      title={submitDisabled ? MSG.supplyDetailsTitle : ""}
+      config={{ display: "block" }}
+    />
+    <FormSubmitNotification
+      status={status === 200 ? "success" : "error"}
+      message={status ? defaultMessage[status] : undefined}
+    />
+  </svelte:fragment>
+</ControlsWrapper>
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { enhance } from "$app/forms";
+  import FormFooter from "../../components/molecular/FormFooter/FormFooter.svelte";
+  import FormWrapper from "../../components/molecular/wrappers/FormWrapper/FormWrapper.svelte";
+  import SignupControls from "../../components/organic/SignupControls/SignupControls.svelte";
+  import { LOGIN_ROUTE } from "../../constants";
+  import { SIGNUP_MESSAGES } from "../../messages";
+  const { login, pageTitle, subtitle, title: formTitle } = SIGNUP_MESSAGES;
+
+  const handleInput = async () => {};
+  let obj = { username: "", email: "", password: "", name: "", surname: "" };
+</script>
+
+<svelte:head><title>{pageTitle}</title></svelte:head>
+<FormWrapper {formTitle} {subtitle}>
+  <form slot="form" method="POST" use:enhance>
+    <SignupControls
+      bind:username={obj.username}
+      bind:password={obj.password}
+      bind:email={obj.email}
+      bind:name={obj.name}
+      bind:surname={obj.surname}
+      onInput={handleInput}
+    />
+  </form>
+  <svelte:fragment slot="footer">
+    <FormFooter link={LOGIN_ROUTE} label={login} />
+  </svelte:fragment>
+</FormWrapper>
