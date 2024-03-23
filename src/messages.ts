953a68896910c338f596adb0396ead82e54c4749
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-03-23 09:33:27 +0100

    Add signup messages

--- src/messages.ts
+++ src/messages.ts
@@ -6,7 +6,7 @@ export const EXPIRATION_MESSAGES = {
 };
 
 export const LOGIN_MESSAGES = {
-    pageTitle: `${APP_NAME} - Login`,
+  pageTitle: `${APP_NAME} - Login`,
   title: "Sign in",
   subtitle: "...and start chatting!",
   signup: "Not registered? Create an account!",
@@ -15,4 +15,19 @@ export const LOGIN_MESSAGES = {
   passwordPlaceholder: "Enter your password",
   loginSuccess: "Login successful!",
   loginFailure: "Username or password not correct!"
+};
+
+export const SIGNUP_MESSAGES = {
+  pageTitle: `${APP_NAME} - Signup`,
+  title: "Create an account",
+  subtitle: LOGIN_MESSAGES.subtitle,
+  login: "Already registered? Try logging in!",
+  usernamePlaceholder: LOGIN_MESSAGES.usernamePlaceholder,
+  emailPlaceholder: "Enter your email",
+  namePlaceholder: "Enter your name (optional)",
+  surnamePlaceholder: "Enter your surname (optional)",
+  passwordPlaceholder: LOGIN_MESSAGES.passwordPlaceholder,
+  signupSuccess: "Successfully registered!",
+  signupFailure: "Registration failed! Please try again or log in, if you already have an account.",
+  supplyDetailsTitle: "Please supply a username, email, and password.",
 };
