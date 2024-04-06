e5ff138d7d39379faca84875b69c649185546173
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-04-06 21:23:02 +0200

    Create basic dialog component and story

---
+++ src/components/molecular/Dialog/Dialog.stories.svelte
@@ -0,0 +1,63 @@
+<script context="module" lang="ts">
+  import type { Meta } from "@storybook/svelte";
+  import { action } from "@storybook/addon-actions";
+
+  import Dialog from "./Dialog.svelte";
+  import type { ComponentProps } from "svelte";
+  export const meta: Meta<ComponentProps<Dialog>> = {
+    title: "Molecular/Dialog",
+    component: Dialog,
+    argTypes: {
+      open: { control: "boolean", table: { disable: true } },
+      confirmAction: { table: { disable: true } },
+      message: { control: "text" },
+      confirmMessage: { control: "text" },
+      rejectMessage: { control: "text" }
+    },
+    args: { confirmAction: action("confirmAction") }
+  };
+</script>
+
+<script lang="ts">
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+  import Button from "../../atomic/Button/Button.svelte";
+  let isOpen = true;
+</script>
+
+<Template let:args>
+  <div class="flex wrapper items-center justify-center border-2 border-violet-500 repeat from-violet-400 to-violet-600">
+    <Button
+      action="cancel"
+      on:click={() => {
+        if (!isOpen) isOpen = true;
+      }}>Open dialog</Button
+    >
+  </div>
+
+  <Dialog
+    {...args}
+    open={isOpen}
+    on:close
+    on:close={() => {
+      isOpen = false;
+    }}
+  />
+</Template>
+
+<Story
+  name="Primary"
+  args={{
+    open: isOpen,
+    message: "Is this what you want, what you really really want?",
+    confirmMessage: "",
+    rejectMessage: ""
+  }}
+/>
+
+<style>
+.wrapper {
+  height: calc(100vh - 2rem);
+  width: calc(100vw - 2rem);
+  background: repeating-linear-gradient(135deg, rgb(167, 139, 250), rgb(167, 139, 250) 20px, lightgray 20px,lightgray 40px )
+}
+</style>
---
+++ src/components/molecular/Dialog/Dialog.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+  import { onDestroy, onMount } from "svelte";
+  import Button from "../../atomic/Button/Button.svelte";
+
+  export let open = false;
+  export let message: string;
+  export let confirmMessage: string | undefined = undefined;
+  export let rejectMessage: string | undefined = undefined;
+
+  export let confirmAction: () => unknown = () => {};
+
+  let ref: HTMLDialogElement | undefined;
+
+  const showModal = (open: boolean) => {
+    if (!ref) return;
+    if (open) {
+      ref.showModal();
+      return;
+    }
+    ref.close();
+  };
+
+  const onClose = () => {
+    if (open) open = false;
+  };
+
+  const handleClick = (val: boolean) => {
+    if (val) confirmAction();
+    open = false;
+  };
+
+  $: showModal(open);
+
+  onMount(() => {
+    ref?.addEventListener("close", onClose);
+    showModal(open);
+  });
+
+  onDestroy(() => {
+    ref?.removeEventListener("close", onClose);
+  });
+</script>
+
+<dialog
+  bind:this={ref}
+  class="scale-75 rounded-md from-blue-100 to-blue-200 opacity-50 shadow transition-all duration-150 backdrop:bg-gradient-to-bl backdrop:opacity-40 [&.open]:scale-100 [&.open]:opacity-100"
+  class:open
+  on:close
+>
+  <div class="mt-2 px-8 py-4">
+    <p class="mb-5 mt-2">{message}</p>
+    <div class="mb-2 mt-3 flex justify-center gap-3">
+      <Button
+        action="danger"
+        size="sm"
+        on:click={() => {
+          handleClick(true);
+        }}>{confirmMessage ? confirmMessage : "Yes"}</Button
+      >
+      {#if open}
+        <Button
+          focus={true}
+          action="cancel"
+          size="sm"
+          on:click={() => {
+            handleClick(false);
+          }}>{rejectMessage ? rejectMessage : "No"}</Button
+        >
+      {/if}
+    </div>
+  </div>
+</dialog>
+
+<style>
+  .shadow {
+    box-shadow: rgba(239, 68, 68, 0.2) 0px 0px 10px 2px;
+  }
+</style>
