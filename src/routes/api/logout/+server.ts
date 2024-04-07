5b10731562f86d4977543ef6f6a0bd1293262b56
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-04-07 18:18:28 +0200

    Update logout endpoint with new session handler callbacks

--- src/routes/api/logout/+server.ts
+++ src/routes/api/logout/+server.ts
@@ -1,36 +1,20 @@
-import { error, type RequestHandler } from "@sveltejs/kit";
-import { SESSION_COOKIE } from "../../../constants.js";
+import { assertSessionCookie, closeSocket, logoutUser } from "$lib/server/disconnect-user.js";
 import { secureCookieEval } from "$lib/utils.js";
 import { redisService } from "@db";
-
-/** Logout the current user. Will not check if session exists.
- * Will not authenticate. This is done by the authentication function called
- * previously in the handle hook. */
-const logoutUser = async (sessionId: string): Promise<boolean> => {
-  const logout = await redisService.deleteSession(sessionId);
-  return !!logout;
-};
+import { error, type RequestHandler } from "@sveltejs/kit";
+import { SESSION_COOKIE } from "../../../constants.js";
 
 export const DELETE: RequestHandler = async ({ cookies, url, locals }) => {
-  const chatSessionId = cookies.get(SESSION_COOKIE);
-  if (!chatSessionId) {
-    throw error(400, "No session id!");
-  }
+  const chatSessionId = assertSessionCookie(cookies);
   cookies.delete(SESSION_COOKIE, { secure: secureCookieEval(url), path: "/" });
   const socketServer = locals.socketServer;
   if (socketServer) {
-    const socketId = await redisService.getSocketSession(chatSessionId);
-    if (socketId) {
-      await redisService.deleteSocketSession(chatSessionId);
-      const sockets = await socketServer.fetchSockets();
-      const targetSocket = sockets.find(({ id }) => id === socketId);
-      targetSocket?.disconnect(true);
-    }
+    await closeSocket(socketServer, redisService, chatSessionId);
   }
-  const logout = await logoutUser(chatSessionId);
+  const logout = await logoutUser(redisService, chatSessionId);
   if (!logout) {
     throw error(500, "Error while performing logout!");
   }
-  
+
   return new Response(null, { status: 200 });
 };
