29bfcee25cf1d15eb0d2f3f7674b4f66a79bb6b1
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-04-07 18:18:37 +0200

    Create delete-account API endpoint

---
+++ src/routes/api/delete-account/+server.ts
@@ -0,0 +1,21 @@
+import { assertSessionCookie, closeSocket, logoutUser } from "$lib/server/disconnect-user";
+import { error, type RequestHandler } from "@sveltejs/kit";
+import { dbService } from "../../../../db/postgres";
+import { redisService } from "../../../../db/redis/redis-service";
+
+export const DELETE: RequestHandler = async ({ cookies, locals }) => {
+  const chatSessionId = assertSessionCookie(cookies);
+  const socketServer = locals.socketServer;
+  const user = await redisService.getSession(chatSessionId);
+  if (!user) {
+    throw error(500, "Could not correlate session id to user!");
+  }
+  await dbService.removeUser(user.id, user.id);
+
+  if (socketServer) {
+    await closeSocket(socketServer, redisService, chatSessionId);
+  }
+  await logoutUser(redisService, chatSessionId);
+
+  return new Response("success", { status: 200 });
+};
