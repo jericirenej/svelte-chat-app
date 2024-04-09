c3eab812ffcd34fcc0413c9e882926915b2b7012
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-04-09 19:40:22 +0200

    Add delete account tests

--- tests/profile.spec.ts
+++ tests/profile.spec.ts
@@ -1,5 +1,10 @@
-import { expect, test } from "@playwright/test";
+import { expect, test, type Page } from "@playwright/test";
+import { dbService } from "../db/postgres/db-service.js";
 import type { AvailableUsers } from "../db/postgres/seed/seed.js";
+import type { CreateUserDto } from "../db/postgres/types.js";
+import { LOGIN_ROUTE } from "../src/constants.js";
+import { PROFILE_MESSAGES } from "../src/messages.js";
+import { genPassword } from "../utils/generate-password.js";
 import { login, userHashMap } from "./utils.js";
 
 const user: AvailableUsers = "logician",
@@ -7,13 +12,17 @@ const user: AvailableUsers = "logician",
 
 const target = userHashMap[user];
 
-test.beforeEach(async ({ page }) => {
+const deleteButton = (page: Page) =>
+  page.getByRole("button", { name: PROFILE_MESSAGES.deleteButton });
+
+const loginUserAndNavigate = async (page: Page, user: string, password: string): Promise<void> => {
   await login(page, user, password);
-  await page.getByRole("link", { name: "Profile" }).click();
-  await page.waitForURL("/profile/");
-});
+  await page.waitForURL("/");
+  await page.goto("/profile");
+};
 
 test("Should display appropriate fields and data", async ({ page, browser, context, locale }) => {
+  await loginUserAndNavigate(page, user, password);
   await expect(page.getByRole("heading")).toContainText("Profile");
   await expect(page.getByRole("paragraph")).toContainText(user);
   const dateFormat = new Intl.DateTimeFormat(locale);
@@ -34,4 +43,72 @@ test("Should display appropriate fields and data", async ({ page, browser, conte
     await expect(labelEl).toBeVisible();
     expect(valueEl).toBe(value);
   }
+  await expect(deleteButton(page)).toBeVisible();
+  await expect(deleteButton(page)).toBeEnabled();
+});
+
+test.describe("Delete account", () => {
+  const createUsername = (browserName: string) => `del_${browserName}`;
+  const password = `delete-user-password`;
+  const { hash, salt } = genPassword(password);
+  test.beforeEach(async ({ browserName }) => {
+    const newUser: CreateUserDto = {
+      email: `${createUsername(browserName)}@nowhere.never`,
+      username: createUsername(browserName),
+      hash,
+      salt
+    };
+    await dbService.addUser(newUser);
+  });
+  test.afterEach(async ({ browserName }) => {
+    const username = `del_${browserName}`;
+    const user = await dbService.getUser({ property: "username", value: username });
+    if (user) {
+      await dbService.removeUser(user.id, user.id);
+    }
+  });
+  test("Clicking on delete account should show dialog", async ({ page, browserName }) => {
+    await loginUserAndNavigate(page, createUsername(browserName), password);
+    await deleteButton(page).click();
+
+    const dialog = page.getByRole("dialog");
+    await expect(dialog).toBeVisible();
+    await expect(
+      dialog.getByRole("heading", { name: PROFILE_MESSAGES.deleteDialogHeading })
+    ).toBeVisible();
+    await expect(dialog.getByText(PROFILE_MESSAGES.deleteMessage)).toBeVisible();
+    await expect(dialog.getByRole("button", { name: "No" })).toBeVisible();
+    await expect(
+      dialog.getByRole("button", { name: PROFILE_MESSAGES.deleteConfirm })
+    ).toBeVisible();
+  });
+  test("Escape press, direct enter press, or clicking the 'No' button should not delete account", async ({
+    page,
+    browserName
+  }) => {
+    await loginUserAndNavigate(page, createUsername(browserName), password);
+    const dialog = page.getByRole("dialog");
+    for (const key of ["Escape", "Enter"]) {
+      await deleteButton(page).click();
+      await expect(dialog).toBeVisible();
+      await page.keyboard.press(key);
+      await expect(dialog).toBeHidden();
+    }
+
+    await deleteButton(page).click();
+    await dialog.getByRole("button", { name: "No" }).click();
+    await expect(dialog).toBeHidden();
+    await expect(deleteButton(page)).toBeEnabled();
+  });
+  test("Confirmation should delete account and redirect to login", async ({
+    page,
+    browserName
+  }) => {
+    await loginUserAndNavigate(page, createUsername(browserName), password);
+    await deleteButton(page).click();
+    await page.getByRole("button", { name: PROFILE_MESSAGES.deleteConfirm }).click();
+    await expect(page).toHaveURL(LOGIN_ROUTE);
+    const userExists = await dbService.usernameExists(createUsername(browserName));
+    expect(userExists).toBeFalsy();
+  });
 });
