1ae050ab431d9669e0df94b9766654f8b8d4bea3
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-04-10 21:18:41 +0200

    Add csrf test and super-admin deletiion tests

--- tests/profile.spec.ts
+++ tests/profile.spec.ts
@@ -2,7 +2,13 @@ import { expect, test, type Page } from "@playwright/test";
 import { dbService } from "../db/postgres/db-service.js";
 import type { AvailableUsers } from "../db/postgres/seed/seed.js";
 import type { CreateUserDto } from "../db/postgres/types.js";
-import { LOGIN_ROUTE } from "../src/constants.js";
+import {
+  CSRF_HEADER,
+  DELETE_ACCOUNT_ROUTE,
+  LOGIN_ROUTE,
+  PROFILE_ROUTE,
+  ROOT_ROUTE
+} from "../src/constants.js";
 import { PROFILE_MESSAGES } from "../src/messages.js";
 import { genPassword } from "../utils/generate-password.js";
 import { login, userHashMap } from "./utils.js";
@@ -17,8 +23,8 @@ const deleteButton = (page: Page) =>
 
 const loginUserAndNavigate = async (page: Page, user: string, password: string): Promise<void> => {
   await login(page, user, password);
-  await page.waitForURL("/");
-  await page.goto("/profile");
+  await page.waitForURL(ROOT_ROUTE);
+  await page.goto(PROFILE_ROUTE);
 };
 
 test("Should display appropriate fields and data", async ({ page, browser, context, locale }) => {
@@ -110,5 +116,42 @@ test.describe("Delete account", () => {
     await expect(page).toHaveURL(LOGIN_ROUTE);
     const userExists = await dbService.usernameExists(createUsername(browserName));
     expect(userExists).toBeFalsy();
+  });
+  test("Delete operation should fail, if csrf header is not present", async ({
+    page,
+    browserName
+  }) => {
+    const resp = page.waitForResponse((resp) => resp.url().includes(DELETE_ACCOUNT_ROUTE));
+    await page.route(DELETE_ACCOUNT_ROUTE, async (route) => {
+      console.log(route.request().url());
+      const headers = route.request().headers();
+      delete headers[CSRF_HEADER.toLowerCase()];
+      await route.continue({ headers });
+    });
+    await loginUserAndNavigate(page, createUsername(browserName), password);
+    await deleteButton(page).click();
+    await page.getByRole("button", { name: PROFILE_MESSAGES.deleteConfirm }).click();
+
+    await expect(page).toHaveURL(new RegExp(PROFILE_ROUTE));
+    const userExists = await dbService.usernameExists(createUsername(browserName));
+    expect(userExists).toBeTruthy();
+    const awaitedResponse = await resp;
+    expect(awaitedResponse.status()).toBe(403);
+  });
+  test("Should not allow super-admins to delete own account", async ({ page }) => {
+    const resp = page.waitForResponse((resp) => resp.url().includes(DELETE_ACCOUNT_ROUTE));
+    await loginUserAndNavigate(
+      page,
+      userHashMap.lovelace.username,
+      `${userHashMap.lovelace.username}-password`
+    );
+    await deleteButton(page).click();
+    await page.getByRole("button", { name: PROFILE_MESSAGES.deleteConfirm }).click();
+    await page.reload();
+    await expect(page).toHaveURL(new RegExp(PROFILE_ROUTE));
+    const userExists = await dbService.usernameExists(userHashMap.lovelace.username);
+    expect(userExists).toBeTruthy();
+    const awaitedResponse = await resp;
+    expect(awaitedResponse.status()).toBe(400);
   });
 });
