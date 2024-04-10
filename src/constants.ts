3c2da5f30cb17aa44ae5eeb927066c22bf880a33
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-04-10 21:17:25 +0200

    Add profile route constant. Modify route matchers in tests and nav component

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 export const ROOT_ROUTE = "/";
 export const LOGIN_ROUTE = "/login";
 export const SIGNUP_ROUTE = "/signup";
+export const PROFILE_ROUTE = "/profile";
 /** This s a list of **exact** routes on which no authentication is required.
  * All other routes should perform a check for `sessionId` cookie and and `X-CRSF` header. */
 export const UNPROTECTED_ROUTES = [LOGIN_ROUTE, SIGNUP_ROUTE];
