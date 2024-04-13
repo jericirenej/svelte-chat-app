2641f69cd36378e31209df53753345513cb2c2d9
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-04-13 18:07:01 +0200

    Add StoryFieldsetWrapper storybook helper

---
+++ src/components/helpers/StoryFieldsetWrapper.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let labelCallback: (() => unknown) | undefined = undefined;
+  export let label: string;
+</script>
+
+<fieldset
+  class="flex
+h-[125px] w-[270px] flex-row items-center
+gap-x-5
+rounded
+border-2
+border-stone-300
+px-4
+pb-2
+"
+>
+  <legend class="select-none">
+    {#if labelCallback}
+      <button
+        class="rounded-md border px-2 text-xs uppercase tracking-wide transition-colors hover:bg-neutral-100"
+        on:click={labelCallback}>{label}</button
+      >
+    {:else}
+      <span class="rounded-md border px-2 text-xs uppercase tracking-wide">{label}</span>
+    {/if}
+  </legend>
+  <slot />
+</fieldset>
