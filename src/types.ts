4bb359f1b53fe04a8a648c2a3fbd0c3d88dc4583
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-04-14 19:50:07 +0200

    Define Notification types in a separate file

--- src/types.ts
+++ src/types.ts
@@ -9,4 +9,9 @@ export type PbkdfSettings = {
 export type RemoveIndexSignature<T extends Record<string, unknown>> = {
   [K in keyof T as string extends K ? never : K]: T[K];
 };
-
+export type NotificationTypes = "default" | "secondary" | "failure";
+export type NotificationEntry = {
+  content: string;
+  action?: (...args: unknown[]) => unknown;
+  type?: NotificationTypes;
+};
