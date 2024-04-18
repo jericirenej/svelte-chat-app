2d8a4244a6d61cf3dbc4f1d07d5a051e99c42ad5
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-04-18 21:06:23 +0200

    Base TypingIndicator component

---
+++ src/components/atomic/TypingIndicator/TypingIndicator.stories.svelte
@@ -0,0 +1,33 @@
+<script context="module" lang="ts">
+  import type { Meta } from "@storybook/svelte";
+  import TypingIndicator from "./TypingIndicator.svelte";
+  import type { ComponentProps } from "svelte";
+
+  export const meta: Meta<ComponentProps<TypingIndicator>> = {
+    title: "Atomic/TypingIndicator",
+    component: TypingIndicator,
+    argTypes: {
+      usersTyping: {
+        control: "check",
+        options: ["Ada", "Alan", "Alonzo", "Barbara", "Charles", "Kurt"]
+      }
+      // debounceTime: { type: "number" }
+    }
+  };
+</script>
+
+<script lang="ts">
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+  import type { RemoveIndexSignature } from "../../../types";
+
+  const assertArgs = (args: unknown) => {
+    return args as RemoveIndexSignature<ComponentProps<TypingIndicator>>;
+  };
+</script>
+
+<Template let:args>
+  {@const storyArgs = assertArgs(args)}
+  <TypingIndicator {...storyArgs} />
+</Template>
+
+<Story name="Primary" args={{ usersTyping: [] }} />
---
+++ src/components/atomic/TypingIndicator/TypingIndicator.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  import { toArray } from "../../../helpers";
+  import type { MaybeArray, Nullish } from "../../../types";
+
+  export let usersTyping: MaybeArray<string> | Nullish = null;
+  // export let debounceTime = 200;
+
+  const handleUsers = (users: typeof usersTyping): string => {
+    const userArr = toArray(users).filter(Boolean) as string[];
+    let subjects = "";
+    if (!userArr.length) return subjects;
+    const verb = userArr.length === 1 ? "is typing" : "are typing";
+    switch (userArr.length) {
+      case 1:
+        subjects = userArr[0];
+        break;
+      case 2:
+        subjects = userArr.join(" and ");
+        break;
+      default:
+        subjects = [userArr.slice(0, -1).join(", "), userArr.at(-1) as string].join(", and ");
+    }
+    return `${subjects} ${verb}`;
+  };
+
+  $: writing = handleUsers(usersTyping);
+</script>
+
+{#if usersTyping?.length}
+  <div
+    in:fade={{ duration: 150 }}
+    out:fade={{ duration: 70 }}
+    class="flex w-fit items-baseline rounded-lg bg-slate-200 px-2 py-1"
+  >
+    <span class="text-xs font-medium text-gray-500">{writing}</span>
+    <div class=" mx-2 flex gap-1">
+      {#each [0, 1, 2] as dot}
+        <div
+          class="dot relative inline-block aspect-square w-[4px] rounded-md bg-slate-500 opacity-0"
+          style:animation-delay={`${dot * 200}ms`}
+        ></div>
+      {/each}
+    </div>
+  </div>
+{/if}
+
+<style>
+  .dot {
+    animation: jump 1s infinite linear;
+  }
+  @keyframes jump {
+    0% {
+      opacity: 0;
+    }
+    45% {
+      opacity: 1;
+    }
+  }
+</style>
