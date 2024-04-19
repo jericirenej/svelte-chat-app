0e9e3528de0013bfc585cfccffd1bdc58fd717cb
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-04-19 22:14:30 +0200

    Refactor animated dots to separate component

--- src/components/atomic/TypingIndicator/TypingIndicator.svelte
+++ src/components/atomic/TypingIndicator/TypingIndicator.svelte
@@ -2,6 +2,7 @@
   import { fade, scale } from "svelte/transition";
   import { toArray } from "../../../helpers";
   import type { MaybeArray, Nullish } from "../../../types";
+  import AnimatedDots from "../AnimatedDots/AnimatedDots.svelte";
 
   export let usersTyping: MaybeArray<string> | Nullish = null;
   // export let debounceTime = 200;
@@ -29,32 +30,11 @@
 
 {#if usersTyping?.length}
   <div
-    in:scale={{ duration: 150 }}
+    in:scale={{ duration: 150}}
     out:fade={{ duration: 70 }}
     class="flex w-fit items-baseline rounded-lg bg-slate-200 px-2 py-1"
   >
     <span class="text-xs font-medium text-gray-500">{writing}</span>
-    <div class=" mx-2 flex gap-1">
-      {#each [0, 1, 2] as dot}
-        <div
-          class="dot relative inline-block aspect-square w-[4px] rounded-md bg-slate-500 opacity-0"
-          style:animation-delay={`${dot * 200}ms`}
-        ></div>
-      {/each}
-    </div>
+    <AnimatedDots />
   </div>
 {/if}
-
-<style>
-  .dot {
-    animation: jump 1s infinite linear;
-  }
-  @keyframes jump {
-    0% {
-      opacity: 0;
-    }
-    45% {
-      opacity: 1;
-    }
-  }
-</style>
