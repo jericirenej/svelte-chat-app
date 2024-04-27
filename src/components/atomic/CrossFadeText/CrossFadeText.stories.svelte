332f7fec1d18f5047d4329949a57a3c7daf81d28
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-04-27 18:12:06 +0200

    Add CrossFadeText component

---
+++ src/components/atomic/CrossFadeText/CrossFadeText.stories.svelte
@@ -0,0 +1,31 @@
+<script context="module" lang="ts">
+  import type { Meta } from "@storybook/svelte";
+  import CrossFadeText from "./CrossFadeText.svelte";
+
+  export const meta: Meta<CrossFadeText> = {
+    title: "Atomic/CrossFadeText",
+    component: CrossFadeText,
+    argTypes: {
+      text: { table: { disable: true } }
+    }
+  };
+</script>
+
+<script lang="ts">
+  import { faker } from "@faker-js/faker";
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+  import Button from "../Button/Button.svelte";
+  let text = "Example";
+  const changeText = () => {
+    text = faker.word.sample({ length: { min: 4, max: 10 } });
+  };
+</script>
+
+<Template>
+  <div class="flex flex-col w-fit">
+    <CrossFadeText {text} />
+    <Button customClasses="mt-7" type="button" action="info" size="sm" variant="outline" display="inline-block" on:click={changeText}>Change</Button>
+  </div>
+</Template>
+
+<Story name="Primary" />
---
+++ src/components/atomic/CrossFadeText/CrossFadeText.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+
+  export let text: string | undefined = undefined;
+  let prevAndCurrent: [string | undefined, string | undefined] = [undefined, undefined];
+  const reconfigure = (current: string | undefined) => {
+    prevAndCurrent = [prevAndCurrent[1], current];
+  };
+
+  $: reconfigure(text);
+</script>
+
+{#if text !== undefined}
+  <div class="relative">
+    <span class="invisible">{text}</span>
+    {#key text}
+      {#if prevAndCurrent[0]}
+        <span class="previous-entry absolute left-0 top-0">{prevAndCurrent[0]}</span>
+      {/if}
+      <span class="absolute left-0 top-0" in:fade={{ duration: 200 }}>{prevAndCurrent[1]}</span>
+    {/key}
+  </div>
+{/if}
+
+<style>
+  .previous-entry {
+    opacity: 1;
+    animation: disappear 300ms forwards;
+  }
+
+  @keyframes disappear {
+    to {
+      opacity: 0;
+    }
+  }
+</style>
