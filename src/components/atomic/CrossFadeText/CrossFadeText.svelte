62d8a301201b6dcc021412626c377c55b39a1c11
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-05-19 17:27:03 +0200

    Add title

--- src/components/atomic/CrossFadeText/CrossFadeText.svelte
+++ src/components/atomic/CrossFadeText/CrossFadeText.svelte
@@ -2,6 +2,7 @@
   import { fade } from "svelte/transition";
 
   export let text: string | undefined = undefined;
+  export let title: string | undefined = undefined;
   let prevAndCurrent: [string | undefined, string | undefined] = [undefined, undefined];
   const reconfigure = (current: string | undefined) => {
     prevAndCurrent = [prevAndCurrent[1], current];
@@ -17,7 +18,7 @@
       {#if prevAndCurrent[0]}
         <span aria-hidden class="previous-entry absolute left-0 top-0">{prevAndCurrent[0]}</span>
       {/if}
-      <p class="absolute left-0 top-0" in:fade={{ duration: 200 }}>{prevAndCurrent[1]}</p>
+      <p class="absolute left-0 top-0" {title} in:fade={{ duration: 200 }}>{prevAndCurrent[1]}</p>
     {/key}
   </div>
 {/if}
