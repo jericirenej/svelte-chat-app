bf4259aa7e279c3877cca8f7802b96ca6f803e4a
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-05-19 17:24:24 +0200

    Add svelte testing library and set related configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import type { PreviewServer, ViteDevServer } from "vite";
 import { defineConfig } from "vitest/config";
 import { createSocketServer } from "./src/lib/server/socket.global.js";
+import { svelteTesting } from "@testing-library/svelte/vite";
 
 export default defineConfig({
   plugins: [
     sveltekit(),
+    svelteTesting(),
     {
       name: "socketIOIntegration",
       configureServer(server: ViteDevServer) {
@@ -18,11 +20,12 @@ export default defineConfig({
   ],
   test: {
     environmentMatchGlobs: [
-      ["src/**/*.spec.ts", "happy-dom"],
+      ["src/components/**/*.spec.ts", "happy-dom"],
       ["db/**/*.spec.ts", "node"],
       ["utils/**/*.spec.ts", "node"],
       ["src/lib/*.ts", "node"]
     ],
+    setupFiles: ["./vitest-setup.ts"],
     include: ["src/**/*.spec.ts", "db/**/*.spec.ts", "utils/**/*.spec.ts"],
     includeSource: ["src/**/*.ts", "db/redis/*.ts", "utils/**/*.spec.ts"]
   }
