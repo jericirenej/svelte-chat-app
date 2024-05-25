f65a6132c4fc388059c3b0bbd8b3eb599ed0fd3b
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-05-25 20:51:27 +0200

    - Update to height handling - Changed colors

--- src/components/atomic/SendIcon/SendIcon.svelte
+++ src/components/atomic/SendIcon/SendIcon.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   export let disabled = true;
+  let ref: HTMLButtonElement;
   import SendIcon from "@iconify/icons-iconoir/send";
   import Icon from "@iconify/svelte";
   import { CONVERSATION_MESSAGES } from "../../../messages";
@@ -8,10 +9,12 @@
 </script>
 
 <button
+  bind:this={ref}
   on:click
+  on:pointerdown
   {title}
   {disabled}
-  class="cursor-pointer rounded-full p-2 transition-shadow hover:bg-neutral-100 hover:shadow-md disabled:cursor-default"
+  class="cursor-pointer rounded-full p-2 transition-shadow hover:bg-neutral-100 hover:shadow-md active:scale-95 active:bg-neutral-100 active:shadow-md active:brightness-95 disabled:scale-100 disabled:cursor-default disabled:bg-transparent disabled:text-gray-500 disabled:shadow-none"
 >
-  <Icon icon={SendIcon} class="text-[25px] text-inherit" />
+  <Icon icon={SendIcon} class="text-[28px] text-current" />
 </button>
--- src/components/atomic/TextArea/TextArea.svelte
+++ src/components/atomic/TextArea/TextArea.svelte
@@ -1,24 +1,28 @@
 <script lang="ts">
   import { CONVERSATION_MESSAGES } from "../../../messages";
-  let ref: HTMLTextAreaElement;
+  let ref: HTMLTextAreaElement | undefined;
 
-  export let value: string | undefined;
+  export let value: string;
   export let onInput: ((ev?: Event) => unknown) | undefined = undefined;
 
-  const handleHeight = () => {
+  const handleHeight = (value: string) => {
+    if (!ref) return;
     ref.style.height = "auto";
-    ref.style.height = `${ref.scrollHeight + 4}px`;
+    if (value.length) {
+      ref.style.height = `${ref.scrollHeight + 4}px`;
+    }
   };
 
   const handleInput = (ev: Event) => {
-    handleHeight();
     onInput && onInput(ev);
   };
+
+  $: handleHeight(value);
 </script>
 
 <textarea
   bind:this={ref}
-  class="w-full resize-none overflow-hidden rounded-md border-2 border-neutral-400 p-[8px] focus-visible:border-violet-500 focus-visible:shadow-violet-500 focus-visible:outline-none"
+  class="focus-visible:shadow-bluborder-blue-500 w-full resize-none overflow-hidden rounded-md border-2 border-neutral-400 p-[8px] focus-visible:border-blue-500 focus-visible:outline-none"
   bind:value
   spellcheck="true"
   placeholder={CONVERSATION_MESSAGES.textPlaceholder}
