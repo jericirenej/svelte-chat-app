3cb11d9f163079ecc4fd7a998a32765ec1f04468
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-06-12 20:18:10 +0200

    Update debounce and add throttle helper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,14 @@
 export const promisifiedTimeout = async (timeout: number) =>
   new Promise((resolve) => setTimeout(resolve, timeout));
 
-export const debounce = <F extends (...args: unknown[]) => ReturnType<F>>(
+type TimeoutReturn = ReturnType<typeof setTimeout>;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const debounce = <F extends (...args: any[]) => ReturnType<F>>(
   callback: F,
   timeout: number
 ) => {
-  let timer: NodeJS.Timeout;
+  let timer: TimeoutReturn | undefined;
 
   return (...args: Parameters<F>) => {
     clearTimeout(timer);
@@ -15,4 +18,35 @@ export const debounce = <F extends (...args: unknown[]) => ReturnType<F>>(
   };
 };
 
-export const secureCookieEval = (url:URL):boolean =>  url.hostname === "localhost" || url.protocol === "https"
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const throttle = <F extends (...args: any[]) => ReturnType<F>>(
+  callback: F,
+  timeout: number
+) => {
+  let inThrottle: boolean = false,
+    lastTime = Date.now(),
+    lastFn: TimeoutReturn | undefined;
+  return (...args: Parameters<F>) => {
+    if (!inThrottle) {
+      callback(...args);
+      lastTime = Date.now();
+      inThrottle = true;
+      return;
+    }
+    clearTimeout(lastFn);
+    const now = Date.now();
+    lastFn = setTimeout(
+      () => {
+        const diff = now - lastTime;
+        if (diff >= timeout) {
+          callback(...args);
+          lastTime = now;
+        }
+      },
+      Math.max(timeout - (now - lastTime), 0)
+    );
+  };
+};
+
+export const secureCookieEval = (url: URL): boolean =>
+  url.hostname === "localhost" || url.protocol === "https";
