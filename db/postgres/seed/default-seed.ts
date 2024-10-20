301405f9016e4baab4b0df5898c9600a38f10e12
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-10-20 12:15:43 +0200

    Message indexing in default seed

--- db/postgres/seed/default-seed.ts
+++ db/postgres/seed/default-seed.ts
@@ -1,18 +1,19 @@
 import { Kysely } from "kysely";
-import { BASE_USERS as users } from "../../../utils/users";
+import { BASE_USERS as users } from "../../../utils/users.js";
 import type { DB } from "../db-types";
 import { type ChatSchema, pickUser, Seeder } from "./seed";
 import MESSAGES from "./seed.messages.js";
 
 type UserNames = (typeof users)[number]["username"];
+const prefixMessage = (msg: string, index: number) => `Msg ${index + 1}: ${msg}`;
 
 const chats: ChatSchema<UserNames>[] = [
   {
     participants: ["lovelace", "the_turing"],
     name: "A conversation between admins",
-    messages: Array.from(Array(4), (_, i) => ({
+    messages: Array.from(Array(64), (_, i) => ({
       username: pickUser<UserNames>(i, "lovelace", "the_turing"),
-      message: MESSAGES[i]
+      message: prefixMessage(MESSAGES[i], i)
     }))
   }
 ] satisfies ChatSchema<UserNames>[];
@@ -20,7 +21,7 @@ chats.push({
   participants: ["incomplete_guy", "chu_lonzo"],
   messages: Array.from(Array(8), (_, i) => ({
     username: pickUser<UserNames>(i, "incomplete_guy", "chu_lonzo"),
-    message: MESSAGES[chats[0].messages.length + i]
+    message: prefixMessage(MESSAGES[chats[0].messages.length + i], i)
   }))
 });
 chats.push({
@@ -28,7 +29,7 @@ chats.push({
   name: "Very hush hush",
   messages: Array.from(Array(4), (_, i) => ({
     username: pickUser<UserNames>(i, "lovelace", "liskov"),
-    message: MESSAGES[chats[1].messages.length + i]
+    message: prefixMessage(MESSAGES[chats[1].messages.length + i], i)
   }))
 });
 
