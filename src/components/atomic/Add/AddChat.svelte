1c787616d2eee585f52b0a96883f4f0586f74097
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-11-22 21:03:32 +0100

    AddChat atom added

---
+++ src/components/atomic/Add/AddChat.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import AddIcon from "@iconify/icons-iconoir/chat-lines";
+  import Icon from "@iconify/svelte";
+  export let disabled = true;
+  export let title: string;
+  export let size: number = 25;
+  $: sizePixels = `${size}px`;
+</script>
+
+<button {disabled} {title} on:click on:pointerdown class="cursor-pointer">
+  <Icon
+    icon={AddIcon}
+    class="pointer-events-none text-current"
+    width={sizePixels}
+    height={sizePixels}
+  />
+</button>
