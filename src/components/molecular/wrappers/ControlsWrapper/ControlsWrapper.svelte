fee1cd00ee6cad41d3162baf8d79a521198991c9
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-11-29 19:59:38 +0100

    ControlsWrapper - allow submit to be on top or bottom

--- src/components/molecular/wrappers/ControlsWrapper/ControlsWrapper.svelte
+++ src/components/molecular/wrappers/ControlsWrapper/ControlsWrapper.svelte
@@ -1,8 +1,19 @@
-<div class="flex min-w-[30ch] flex-initial flex-col gap-4">
+<script lang="ts">
+  import clsx from "clsx";
+
+  export let controlOnTop = false;
+  $: classes = clsx({
+    "flex min-w-[30ch] flex-initial gap-4": true,
+    "flex-col": !controlOnTop,
+    "flex-col-reverse": controlOnTop
+  });
+</script>
+
+<div class={classes}>
   <div class="flex flex-col gap-4">
     <slot name="inputs" />
   </div>
-  <div class="relative pb-5">
+  <div class="relative">
     <slot name="controls" />
   </div>
 </div>
