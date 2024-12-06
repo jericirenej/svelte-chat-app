f0963c2eba8cac2869cad16b09a627a1132fde6a
jericirenej <63595376+jericirenej@users.noreply.github.com>
2024-12-06 18:30:38 +0100

    Add login fixture

--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -1,14 +1,18 @@
 /* eslint-disable no-empty-pattern */
-import { test as base } from "@playwright/test";
 import type { DatabaseService } from "@db/postgres/db-service";
+import type { SeedSchema } from "@db/postgres/tools/testing.database.service";
+import { test as base, expect, type Page } from "@playwright/test";
 import { BASE_PORT, WORKERS } from "../playwright.config";
-import { dbName, e2eDatabases } from "./utils";
+import { LOGIN_ROUTE, ROOT_ROUTE } from "../src/constants";
+import { LOGIN_MESSAGES } from "../src/messages";
+import { dbName, e2eDatabases, sleep } from "./utils";
 
 type CustomFixtures = {
   dbService: DatabaseService;
   clearDB: () => Promise<void>;
-  seedDB: () => Promise<void>;
-  seedAll: () => Promise<void>;
+  seedDB: <T extends string>(schema?: SeedSchema<T>) => Promise<void>;
+  seedAll: <T extends string>(schema?: SeedSchema<T>) => Promise<void>;
+  login: (user: string, options?: { waitForRoot?: boolean; page: Page }) => Promise<void>;
 };
 
 export const test = base.extend<CustomFixtures>({
@@ -16,10 +20,10 @@ export const test = base.extend<CustomFixtures>({
     await use(await e2eDatabases.dbService(dbName(base.info())));
   },
   seedAll: async ({}, use) => {
-    const seedAll = async () => {
+    const seedAll = async <T extends string>(schema?: SeedSchema<T>) => {
       await Promise.all(
         Array.from(Array(WORKERS), (_, i) => i + 1).map(async (i) => {
-          await e2eDatabases.seedDbDispose(dbName(i));
+          await e2eDatabases.seedDbDispose(dbName(i), schema);
         })
       );
     };
@@ -30,10 +34,39 @@ export const test = base.extend<CustomFixtures>({
     await use(clearDb);
   },
   seedDB: async ({}, use) => {
-    const seedDb = () => e2eDatabases.seedDbDispose(dbName(base.info()));
+    const seedDb = <T extends string>(schema?: SeedSchema<T>) =>
+      e2eDatabases.seedDbDispose(dbName(base.info()), schema);
     await use(seedDb);
   },
-
+  login: async ({ page }, use) => {
+    const login = async (user: string, options?: { waitForRoot?: boolean; page?: Page }) => {
+      const targetPage = options?.page ?? page,
+        waitForRoot = options?.waitForRoot ?? true;
+      if (!targetPage.url().includes("login")) {
+        await targetPage.goto(LOGIN_ROUTE);
+      }
+      await expect(targetPage).toHaveURL(LOGIN_ROUTE);
+      await targetPage.waitForLoadState("networkidle");
+      const userField = targetPage.getByPlaceholder(LOGIN_MESSAGES.usernamePlaceholder),
+        passwordField = targetPage.getByPlaceholder(LOGIN_MESSAGES.passwordPlaceholder);
+      for (const [field, val] of [
+        [userField, user],
+        [passwordField, `${user}-password`]
+      ] as const) {
+        await field.click({ delay: 50 });
+        await field.fill(val);
+        await sleep(30);
+        await field.press("Tab");
+      }
+      const button = targetPage.getByRole("button", { name: "SUBMIT" });
+      await expect(button).toBeEnabled();
+      await button.click();
+      if (waitForRoot) {
+        await expect(targetPage).toHaveURL(ROOT_ROUTE);
+      }
+    };
+    await use(login);
+  },
   baseURL: async ({}, use) => {
     await use(`http://localhost:${BASE_PORT + base.info().parallelIndex}`);
   }
