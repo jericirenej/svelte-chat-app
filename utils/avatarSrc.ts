3c463097cc770efc5812b304e841c78644ac4d98
jericirenej <63595376+jericirenej@users.noreply.github.com>
2025-02-15 19:47:29 +0100

    Place attribution for avatars in separate file

--- utils/avatarSrc.ts
+++ utils/avatarSrc.ts
@@ -5,7 +5,5 @@ const baseDir = import.meta.dirname;
 export const avatarTypes = {
   empty: undefined,
   transparentBg: resolve(baseDir, "avatars/transparentBg.webp"),
-  /** Abby avatar converted to base64 and pulled from [Dicebear.com](https://www.dicebear.com/playground/) which is a remix of:
-   * Adventurer Neutral by Lisa Wischofsky, licensed under CC BY 4.0*/
   full: resolve(baseDir, "avatars/full.webp")
 } satisfies Record<AvatarTypeKeys, string | undefined>;
